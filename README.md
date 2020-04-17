222f6f8751fb7fda08f921315dc50f67c4b4f75b
Gianluigi Vitale <gianluigi.vitale11@gmail.com>
2020-04-17 16:09:00 +0200

    New Challenges:
    2. Even or Odd
    3. Sum of positive
    4. Opposite number
    5. Remove First and Last Character
    6. Return Negative
    7. String repeat
    8. Remove String Spaces
    9. Find the smallest integer in the array
    10. Grasshopper - Summation
    11. Convert boolean values to strings 'Yes' or 'No'.
    12. Reversed Strings

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 ### The challenges are taken from codewars.com
 ***
 **To look for a specific challenge just search in the PHP file for 'Challenge Name'**  
-_For Example: to find the 1st challenge (Multiply) just search for: Multiply_
+_For Example: to find the 10th challenge (Grasshopper - Summation) just search for: Grasshopper - Summation_
 ***
 _The challenges are organized in a chronological order_
 
 ### Challenge names:
 1. Multiply
+2. Even or Odd
+3. Sum of positive
+4. Opposite number
+5. Remove First and Last Character
+6. Return Negative
+7. String repeat
+8. Remove String Spaces
+9. Find the smallest integer in the array
+10. Grasshopper - Summation
+11. Convert boolean values to strings 'Yes' or 'No'.
+12. Reversed Strings
--- index.php
+++ index.php
@@ -18,6 +18,282 @@
 // }
 //-----------------------------------------------------------------------------------------------------
 
+//-----------------------------------------------------------------------------------------------------
+    /* 2. Even or Odd
+
+        >Create a function that takes an integer as an argument and returns "Even" for even numbers
+         or "Odd" for odd numbers.
+        EXAMPLE:
+        NOTES:
+    */
+
+// $n = 2;
+//
+// $funzione = even_or_odd($n);
+// echo $funzione;
+//
+// function even_or_odd(int $n): string {
+//     if ($n % 2 == 0) {
+//         return 'Even';
+//     } else {
+//         return 'Odd';
+//     }
+// }
+//-----------------------------------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------------------------------
+    /* 3. Sum of positive
+
+        >You get an array of numbers, return the sum of all of the positives ones.
+        EXAMPLE:
+            [1,-4,7,12] => 1 + 7 + 12 = 20
+        NOTES:
+            if there is nothing to sum, the sum is default to 0.
+    */
+
+// $arr = [-1, 2, 3, 4, -5];
+//
+// $funzione = positive_sum($arr);
+// echo $funzione;
+//
+// function positive_sum($arr) {
+//     if (empty($arr)) {
+//         return 0;
+//     } else {
+//         $sum = 0;
+//         foreach ($arr as $key => $value) {
+//             if ($value > 0) {
+//                 $sum += $value;
+//             }
+//         }
+//
+//         return $sum;
+//     }
+// }
+//-----------------------------------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------------------------------
+    /* 4. Opposite number
+
+        >Very simple, given a number, find its opposite.
+        EXAMPLE:
+            1: -1
+            14: -14
+            -34: 34
+        NOTES:
+    */
+
+// $n = 14;
+//
+// $funzione = opposite($n);
+// echo $funzione;
+//
+// function opposite($n) {
+//     return $n * -1;
+// }
+//-----------------------------------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------------------------------
+    /* 5. Remove First and Last Character
+
+        >Your goal is to create a function that removes the first and last characters of a string.
+         You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+        EXAMPLE:
+        NOTES:
+    */
+
+// $s = 'place';
+//
+// $funzione = remove_char($s);
+// echo $funzione;
+//
+// function remove_char(string $s): string {
+//     return substr($s, 1, -1);
+// }
+//-----------------------------------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------------------------------
+    /* 6. Return Negative
+
+        >In this simple assignment you are given a number and have to make it negative.
+         But maybe the number is already negative?
+        EXAMPLE:
+            makeNegative(1)    // return -1
+            makeNegative(-5)   // return -5
+            makeNegative(0)    // return 0
+            makeNegative(0.12) // return -0.12
+        NOTES:
+            The number can be negative already, in which case no change is required.
+        Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+    */
+
+// $num = -20;
+//
+// $funzione = makeNegative($num);
+// echo $funzione;
+//
+// function makeNegative(float $num) : float {
+//     if ($num <= 0) {
+//         return $num;
+//     }
+//
+//     return $num * -1;
+// }
+//-----------------------------------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------------------------------
+    /* 7. String repeat
+
+        >Write a function called repeatString which repeats the given String src exactly count times.
+        EXAMPLE:
+            repeatStr(6, "I") // "IIIIII"
+            repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+        NOTES:
+    */
+
+// $n = 3;
+// $str = '*';
+//
+// $funzione = repeatStr($n, $str);
+// echo $funzione;
+//
+// function repeatStr($n, $str)
+// {
+//     $new_string = '';
+//     for ($i = 1; $i <= $n; $i++) {
+//         $new_string .= $str;
+//     }
+//
+//     return $new_string;
+// }
+//-----------------------------------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------------------------------
+    /* 8. Remove String Spaces
+
+        >Simple, remove the spaces from the string, then return the resultant string.
+        EXAMPLE:
+        NOTES:
+    */
+
+// $s = '8 j 8   mBliB8g  imjB8B8  jl  B';
+//
+// $funzione = no_space($s);
+// echo $funzione;
+//
+// function no_space(string $s): string {
+//     return str_replace(' ', '', $s);
+// }
+//-----------------------------------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------------------------------
+    /* 9. Find the smallest integer in the array
+
+        >Given an array of integers your solution should find the smallest integer.
+        EXAMPLE:
+            Given [34, 15, 88, 2] your solution will return 2
+            Given [34, -345, -1, 100] your solution will return -345
+        NOTES:
+        You can assume, for the purpose of this kata, that the supplied array will not be empty.
+    */
+
+// $arr = [3, 5, 10, 1, 4, 55];
+//
+// $funzione = smallestInteger($arr);
+// echo $funzione;
+//
+// function smallestInteger($arr) {
+//     $smallest_number = $arr[0];
+//
+//     foreach ($arr as $key => $value) {
+//         if ($value < $smallest_number) {
+//             $smallest_number = $value;
+//         }
+//     }
+//
+//     return $smallest_number;
+// }
+//-----------------------------------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------------------------------
+    /* 10. Grasshopper - Summation
+
+        >Write a program that finds the summation of every number from 1 to num.
+         The number will always be a positive integer greater than 0.
+        EXAMPLE:
+            summation(2) -> 3
+            1 + 2
+            summation(8) -> 36
+            1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+        NOTES:
+    */
+
+// $n = 8;
+//
+// $funzione = summation($n);
+// echo $funzione;
+//
+// function summation($n) {
+//     $sum = 0;
+//
+//     for ($i = $n; $i >= 1; $i--) {
+//         $sum += $i;
+//     }
+//
+//     return $sum;
+// }
+//-----------------------------------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------------------------------
+    /* 11. Convert boolean values to strings 'Yes' or 'No'.
+
+        >Write a function that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+        EXAMPLE:
+        NOTES:
+    */
+
+// $bool = true;
+//
+// $funzione = boolToWord($bool);
+// echo $funzione;
+//
+// function boolToWord($bool) {
+//     if ($bool === true) {
+//         return 'Yes';
+//     }
+//
+//     return 'No';
+// }
+//-----------------------------------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------------------------------
+    /* 12. Reversed Strings
+
+        >Write a function so that it reverses the string value passed into it.
+        EXAMPLE:
+        NOTES:
+    */
+
+// $str = 'world';
+//
+// $funzione = solution($str);
+// echo $funzione;
+//
+//
+// function solution($str) {
+//     return strrev($str);
+// }
+// function solution($str) {   // another way
+//     $reversed_string = '';
+//
+//     for ($i = strlen($str); $i >= 0; $i--) {
+//         $reversed_string .= $str[$i];
+//     }
+//
+//     return $reversed_string;
+// }
+//-----------------------------------------------------------------------------------------------------
+
 
 
 ?>
